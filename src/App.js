54e7375648b8d452234782208d3a296452fee4e6
moshiuramit <moshiur.amit@gmail.com>
2023-07-30 15:13:19 +0600

    update final option

--- src/App.js
+++ src/App.js
@@ -1,19 +1,76 @@
+import { useReducer } from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Outlet,
+} from 'react-router-dom';
+import { AppProvider, ThemeProvider } from './context';
+import { Header, Footer } from './components';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
+//import Main from './components/Main';
+import { Home, BookingPage, ConfirmedBooking } from './pages';
 
-function App() {
+const Root = () => {
   return (
-    <>  {/* React fragment tag */}
-    <Header />
-    <Main />
-    <Footer />
-    <div className="App">
-
-    </div>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   );
+};
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="bookings" element={<BookingPage />}>
+        <Route path="thank-you" element={<ConfirmedBooking />} />
+      </Route>
+    </Route>
+  )
+);
+
+function App() {
+  const initialAppState = {
+    previousLocation: ['/'],
+  };
+  const reducer = (state, { type, payload }) => {
+    switch (type) {
+      case 'setPreviousLocation': {
+        return {
+          // ...state,
+          // previousLocation: [...state.previousLocation]
+          //   .slice(0, 1)
+          //   .push(payload),
+        };
+      }
+
+      default:
+        break;
+    }
+  };
+  const [stateGlobal, dispatchGlobal] = useReducer(reducer, initialAppState);
+  return (
+    <AppProvider value={{ stateGlobal, dispatchGlobal }}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </AppProvider>
+  );
 }
+// function App() {
+//   return (
+//     <>  {/* React fragment tag */}
+//     <Header />
+//     <Main />
+//     <Footer />
+//     <div className="App">
+
+//     </div>
+//     </>
+//   );
+// }
 
 export default App;
